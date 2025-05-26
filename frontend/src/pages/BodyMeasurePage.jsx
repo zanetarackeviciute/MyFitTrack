db081d3c3478eabfa1e0c02ad03ced21e557ab99
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-05-26 16:12:43 +0300

    Add structure for body measure and progress pages

--- frontend/src/pages/BodyMeasurePage.jsx
+++ frontend/src/pages/BodyMeasurePage.jsx
@@ -1,6 +1,103 @@
+import { useRef, useState } from "react"
+import { useNavigate } from "react-router-dom";
+
 const BodyMeasurePage = () => {
+    const navigate = useNavigate()
+
+    const dateRef = useRef()
+    const weightRef = useRef()
+    const chestRef = useRef()
+    const waistRef = useRef()
+    const hipsRef = useRef()
+    const tighsRef = useRef()
+    const bicepsRef = useRef()
+
+    const [matmenys, setMatmenys] = useState()
+
+    const measureHandler = (event) => {
+        event.preventDefault()
+
+        const newMeasure = {
+            date: dateRef.current.value,
+            weight: weightRef.current.valueAsNumber,
+            chest: chestRef.current.valueAsNumber,
+            waist: waistRef.current.valueAsNumber,
+            hips: hipsRef.current.valueAsNumber,
+            tighs: tighsRef.current.valueAsNumber,
+            biceps: bicepsRef.current.valueAsNumber
+        }
+        setMatmenys(newMeasure)
+        
+        fetch('http://localhost:3000/body-measure', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newMeasure)
+        })
+            .then(res => {
+                if (res.status == 201) {
+                    return res.json()
+                }
+            })
+            .then(data => {
+                console.log(data)
+                navigate('/progress')
+            })
+    }
+
+
     return (
-      <div>BodyMeasurePage</div>
+        <div>
+            <h2>Sek savo kūno pokyčius</h2>
+            <h5>Įvesk naujausius matmenis ir stebėk progresą laikui bėgant</h5>
+            <form onSubmit={measureHandler} style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+                <div style={{display: 'flex', gap: '10px'}}>
+                    <label htmlFor="measureDate">Pasirinkite datą</label>
+                    <input ref={dateRef}  type="date" />
+                </div>
+                <div style={{display: 'flex', gap: '10px'}}>
+                    <label htmlFor="weightInput">Svoris</label>
+                    <input ref={weightRef} type="number" min={30} max={600} id="weightInput"/>
+                </div>
+                <div style={{display: 'flex', gap: '10px'}}>
+                    <label htmlFor="chestInput">Krūtinė</label>
+                    <input ref={chestRef} type="number" min={20} max={300} id="chestInput"/>
+                </div>
+                <div style={{display: 'flex', gap: '10px'}}>
+                    <label htmlFor="waistInput">Liemuo</label>
+                    <input ref={waistRef} type="number" min={20} max={300} id="waistInput"/>
+                </div>
+                <div style={{display: 'flex', gap: '10px'}}>
+                    <label htmlFor="hipsInput">Klubai</label>
+                    <input ref={hipsRef} type="number" min={20} max={300} id="hipsInput"/>
+                </div>
+                <div style={{display: 'flex', gap: '10px'}}>
+                    <label htmlFor="tighsInput">Šlaunis</label>
+                    <input  ref={tighsRef}type="number" min={20} max={300} id="tighsInput"/>
+                </div>
+                <div style={{display: 'flex', gap: '10px'}}>
+                    <label htmlFor="bicepsInput">Bicepsas</label>
+                    <input ref={bicepsRef} type="number" min={20} max={300} id="bicepsInput"/>
+                </div>
+                <div>
+                    <button type="submit" style={{padding: '10px 20px', cursor: 'pointer'}}>Išsaugoti matmenis</button>
+                </div>
+            </form>
+                {   
+                    matmenys &&
+                    <div>
+                        <p>Paskutiniai įvesties matmenys:</p>
+                        <p>Įvesties datą: {matmenys.data}</p>
+                        <p>Svoris: {matmenys.svoris}</p>
+                        <p>Krūtinė: {matmenys.krutine}</p>
+                        <p>Liemuo: {matmenys.liemuo}</p>
+                        <p>Klubai: {matmenys.klubai}</p>
+                        <p>Šlaunis: {matmenys.slaunis}</p>
+                        <p>Bicepsas: {matmenys.bicepsas}</p>
+                    </div>
+                }
+        </div>
     )
 }
 
--- frontend/src/pages/ProgressPage.jsx
+++ frontend/src/pages/ProgressPage.jsx
@@ -1,7 +1,69 @@
+import { useState, useEffect } from "react"
+
 const ProgressPage = () => {
+    const [progressData, setProgressData] = useState()
+
+    useEffect(() => {
+        fetch('http://localhost:3000/progress')
+        .then((res) => res.json())
+        .then((data) => setProgressData(data))
+        .catch ((err) => console.error('Klaida', err))
+    }, [])
+    
     return (
-        <div>ProgressPage</div>
+        <div>
+            <h2>Stebėk savo kūno pokyčius</h2>
+            {
+                progressData && progressData.users.length > 0 ? (
+                <div>
+                    <h4>Jūsų matmenys</h4>
+                    {/* {
+                        progressData.users.map((user, index) => {
+                            return (
+                            <div key={index}>
+                                <p>Amžius: {user.age} m</p>
+                                <p>Svoris: {user.weight} kg</p>
+                                <p>Ūgis: {user.height} cm</p>
+                                <p>BMR: {user.bmr}</p>
+                                <p>KMI: {user.kmi}</p>
+                            </div>
+                            )
+                        })
+                    } */}
+
+                    
+                    {
+                        progressData.measures.length > 0 &&
+                        <>
+                            {/* <h3>Matmenys:</h3> */}
+                            <div style={{display: 'flex', flexWrap: 'wrap', gap: '20px'}}>
+                            { progressData.measures.map((measure) => (
+                                    <div key={measure._id} style={{
+                                        borderRadius: '8px',
+                                        padding: '20px',
+                                        marginBottom: '20px',
+                                        backgroundColor: 'aliceblue',
+                                        width: '300px'
+                                      }}>
+                                        <p>Data: {new Date(measure.date).toLocaleDateString()}</p>
+                                        <p>Svoris: {measure.weight} kg</p>
+                                        <p>Krūtinė: {measure.chest} cm</p>
+                                        <p>Liemuo: {measure.waist} cm</p>
+                                        <p>Klubai: {measure.hips} cm</p> 
+                                        <p>Šlaunis: {measure.tighs} cm</p>
+                                        <p>Bicepsas: {measure.biceps} cm</p>
+                                    </div>
+                            ))}
+                            </div>
+                        </>
+                    }
+                </div>
+            ) : (
+                <p>Kol kas nėra duomenų...</p>
+            )}
+        </div>
     )
 }
 
 export default ProgressPage
+
