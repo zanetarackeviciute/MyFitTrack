37d5d23de75eae8b1b0128e6d50c6dc26cfcad72
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-05-26 16:27:37 +0300

    Add structure for about page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,6 +1,10 @@
 const AboutPage = () => {
     return (
-        <div>AboutPage</div>
+        <div>
+            <h2>Tavo kelionė link geresnės fizinės formos - paprasta ir smagi!</h2>
+
+            <p>Ši sporto aplikacija - tavo asmeninis treneris kišenėje! Čia gali lengvai apskaičiuoti dienos kalorijų normą, makroelementų kiekį, sekti kūno pokyčius ir pamatyti, kaip tavo matmenys kinta laikui bėgant. Taip pat gali užsirašyti savo treniruotes, įvesti intensyvumą ir laiką, o vėliau stebėti sudegintų kalorijų kiekį ir istoriją. </p>
+        </div>
     )
 }
 
