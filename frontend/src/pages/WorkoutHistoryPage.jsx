d95490ec03e362c48e7d8977532a83a548516c22
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-05-26 16:11:56 +0300

    Add structure for workouts history page

--- frontend/src/pages/WorkoutHistoryPage.jsx
+++ frontend/src/pages/WorkoutHistoryPage.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react"
+
 const WorkoutHistoryPage = () => {
-    return (
-        <div>WorkoutHistoryPage</div>
-    )
+    const [workouts, setWorkouts] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:3000/progress')
+        .then((res) => res.json())
+        .then((data) => setWorkouts(data.workouts))
+    }, [])
+
+  return (
+    <div style={{display: 'flex', flexWrap: 'wrap', gap: '20px'}}>
+        {
+            workouts.map((workout) => (
+                <div key={workout._id} style={{
+                    borderRadius: '8px',
+                    padding: '20px',
+                    marginBottom: '20px',
+                    backgroundColor: 'aliceblue',
+                    width: '300px'}}>
+                    <p>{new Date(workout.date).toLocaleDateString('lt-LT')}</p>
+                    <p>Treniruotė - {workout.activity}</p>
+                    <p>Trukmė - {workout.duration} min</p>
+                    <p>Sudeginta {workout.burnedCalories} kcal</p>
+                </div>
+            ))
+        }
+        {
+            workouts.length == 0 && 
+            <p>Kol kas nėra jokių iššsaugotų treniruočių</p>
+        }
+    </div>
+  )
 }
 
 export default WorkoutHistoryPage
