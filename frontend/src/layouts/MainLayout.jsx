56d286430009157e4dba996454c8aee0d5073d93
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-05-28 11:24:47 +0300

    Add hamburger function for navigation section

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -5,12 +5,15 @@ import logo from '../assets/MyFitTrackLogo.png'
 const MainLayout = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-xl bg-dark">
+            <nav className="navbar navbar-expand-lg bg-dark">
                 <div className= "container">
                     <div className="logo">
                         <NavLink to='/' className="navbar-brand"><img src={logo} alt="logo" className="logoImg"/></NavLink>
                     </div>
-                    <div className="collapse navbar-collapse">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-label="Expand Navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="nav">
                         <ul className="navbar-nav">
                             <li className="nav-item active" aria-current="page"><NavLink to='/'>Home</NavLink></li>
                             <li className="nav-item"><NavLink to='/new-workout'>New workout</NavLink></li>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './styles/main.scss'
 import App from './App.jsx'
 
