f95171b77b506b9ff10f21c506f31d04254e3d88
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-06-09 12:39:21 +0300

    add and connect mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://127.0.0.1:27017/myFitTrack')
+
+const database = mongoose.connection
+
+database.on('error', (err) => console.log())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
