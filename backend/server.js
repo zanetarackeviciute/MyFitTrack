04b6a6cc4671202c498b76a3480e5be98e6252cd
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-06-09 15:34:57 +0300

    Add routes for workouts and body measurements

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 mongoose.connect('mongodb://127.0.0.1:27017/myFitTrack')
 
 const database = mongoose.connection
@@ -48,40 +50,63 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/workout-history', (req, res) => {
-    res.send({message: 'all workouts'})
-})
-app.get('/workout-history/:id', (req, res) => {
-    res.send({message: 'single workout'})
-})
-app.post('/workout-history/:id', (req, res) => {
-    res.send({message: 'create workout'})
-})
-app.put('/workout-history/:id', (req, res) => {
-    res.send({message: 'update workout'})
-})
-app.delete('/workout-history/:id', (req, res) => {
-    res.send({message: 'delete workout'})
+app.get('/workout-history', async (req, res) => {
+    // res.send({message: 'all workouts'})
+    const workouts = await Workout.find()
+    return res.send(workouts)
 })
 
-app.get('/progress', (req, res) => {
-    res.send({message: 'all measurements'})
+app.get('/workout-history/:id', async (req, res) => {
+    const workout = await Workout.findById(req.params.id)
+    // res.send({message: 'single workout', id: req.params.id})
+    return res.send(workout)
 })
-app.get('/progress/:id', (req, res) => {
-    res.send({message: 'single measurement'})
+
+app.post('/workout-history', async (req, res) => {
+    const newWorkoutData = new Workout(req.body)
+    const insertedDocument = await newWorkoutData.save()
+    return res.status(201).send(insertedDocument)
 })
-app.post('/progress/:id', (req, res) => {
+
+app.put('/workout-history/:id', async (req, res) => {
+    const workout = await Workout.findByIdAndUpdate(req.params.id, req.body, {new: true})    // new: true - kad iskart atvaizduotu nauja irasa
+    // res.send({message: 'update workout'})
+    return res.send(workout)
+})
+
+app.delete('/workout-history/:id', async (req, res) => {
+    const workout = await Workout.findByIdAndDelete(req.params.id)
+    // res.send({message: 'delete workout'})
+    return res.send({message: 'document was deleted'})
+})
+
+app.get('/progress', async (req, res) => {
+    const progress = await Measure.find()
+    return res.send(progress)
+})
+
+app.get('/progress/:id', async (req, res) => {
+    const measure = await Measure.findById(req.params.id)
+    return res.send(measure)
+})
+
+app.post('/progress/', async (req, res) => {
+    const newMeasure = new Measure(req.body)
+    const insertedDocument = await newMeasure.save()
     res.send({message: 'create measurement'})
-})
-app.put('/progress/:id', (req, res) => {
-    res.send({message: 'update measurement'})
-})
-app.delete('/progress/:id', (req, res) => {
-    res.send({message: 'delete measurement'})
+    return res.status(201).send(insertedDocument)
 })
 
+app.put('/progress/:id', async (req, res) => {
+    const measure = await Measure.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    // res.send({message: 'update measurement'})
+    return res.send(measure)
+})
 
-
+app.delete('/progress/:id', async (req, res) => {
+    const measure = await Measure.findByIdAndDelete(req.params.id)
+    return res.send('document was deleted')
+})
 
 
 app.listen(port, () => {
