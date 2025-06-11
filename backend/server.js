531c6d0b9636bbfdd515478ae45148079fa1adfd
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-06-11 16:50:06 +0300

    Fix a bug with loading progress page; change data type in endpoints' responds

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
 app.use(express.json())
 
@@ -62,19 +65,19 @@ app.get('/workout-history', async (req, res) => {
 app.get('/workout-history/:id', async (req, res) => {
     const workout = await Workout.findById(req.params.id)
     // res.send({message: 'single workout', id: req.params.id})
-    return res.send(workout)
+    return res.send({workout})
 })
 
 app.post('/workout-history', async (req, res) => {
     const newWorkoutData = new Workout(req.body)
     const insertedDocument = await newWorkoutData.save()
-    return res.status(201).send(insertedDocument)
+    return res.status(201).send({insertedDocument})
 })
 
 app.put('/workout-history/:id', async (req, res) => {
     const workout = await Workout.findByIdAndUpdate(req.params.id, req.body, {new: true})    // new: true - kad iskart atvaizduotu nauja irasa
     // res.send({message: 'update workout'})
-    return res.send(workout)
+    return res.send({workout})
 })
 
 app.delete('/workout-history/:id', async (req, res) => {
@@ -85,32 +88,33 @@ app.delete('/workout-history/:id', async (req, res) => {
 
 app.get('/progress', async (req, res) => {
     const progress = await Measure.find()
-    return res.send(progress)
+    return res.send({progress})
 })
 
 app.get('/progress/:id', async (req, res) => {
     const measure = await Measure.findById(req.params.id)
-    return res.send(measure)
+    return res.send({measure})
 })
 
-app.post('/progress/', async (req, res) => {
+app.post('/progress', async (req, res) => {
+    try {
     const newMeasure = new Measure(req.body)
     const insertedDocument = await newMeasure.save()
-    res.send({message: 'create measurement'})
-    return res.status(201).send(insertedDocument)
-})
+    return res.status(201).send({insertedDocument})
+    } catch (err) {
+    return res.status(500).send({error: err.message})
+}})
 
 app.put('/progress/:id', async (req, res) => {
     const measure = await Measure.findByIdAndUpdate(req.params.id, req.body, {new: true})
     // res.send({message: 'update measurement'})
-    return res.send(measure)
+    return res.send({measure})
 })
 
 app.delete('/progress/:id', async (req, res) => {
     const measure = await Measure.findByIdAndDelete(req.params.id)
-    return res.send('document was deleted')
+    return res.send({message: 'document was deleted'})
 })
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
