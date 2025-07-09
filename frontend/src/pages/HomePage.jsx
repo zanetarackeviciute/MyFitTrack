c035aee6b9ea05767646ddca1942677ef5fcec8b
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-07-09 21:18:27 +0300

    Add registration and sign in forms

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
+import { useRef } from 'react'
 import './HomePage.scss'
 // import logo from '../assets/MyFitTrackLogo.png'
 const HomePage = () => {
-    
+    const regPasswordRef = useRef()
+    const regEmailRef = useRef()
+
+    const handleRegister = () => {
+        
+    }
     return (
         <div className='home-text'>
             <div className='welcome-text'>
@@ -10,11 +16,50 @@ const HomePage = () => {
                 <h4>Pradėk naują treniruotę, sek savo kūno apimtis arba peržiūrėk pažangą.</h4>
             </div>
             <div className='buttons'>
-                <button>Registruotis</button>
+                <button onClick={handleRegister}>Registruotis</button>
                 <button>Prisijungti</button>
             </div>
+            <div className='registration'>
+                <form action="onSubmit">
+                    <div class="mb-3 row">
+                        <label for="staticEmail" class="col-sm-2 col-form-label">Įveskite el. paštą</label>
+                        <div class="col-sm-10">
+                            <input type="text" ref={regEmailRef} readonly class="form-control-plaintext" id="staticEmail" value="email@pavyzdys.com"/>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Sukurkite slaptažodį: </label>
+                        <div class="col-sm-10">
+                            <input type="password" ref={regPasswordRef} class="form-control" id="inputPassword" aria-describedby="passwordHelpBlock"/>
+                            <div id="passwordHelpBlock" class="form-text">
+                                Slaptažodį turi sudaryti 8-20 simbolių , gali būti raidės ar skaičiai. Be tarpų, specialiųjų simbolių ar emoji.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Pakartokite slaptažodį: </label>
+                        <div class="col-sm-10">
+                            <input type="password" ref={regPasswordRef} class="form-control" id="inputPassword" aria-describedby="passwordHelpBlock"/>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary">Registruotis</button>
+                    </div>
+                </form>
+            </div>
 
-            
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+                <label for="floatingInput">El. paštas</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
+                <label for="floatingPassword">Slaptažodis</label>
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Įeiti</button>
+            </div>
+    
             
         </div>
 
