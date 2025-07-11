73e58ae2ce413774570a25b61b37cb0c2106accb
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-07-11 13:12:25 +0300

    Implement close on overlay click for sign-in and registration forms

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,14 +7,26 @@ const HomePage = () => {
 
     const [showRegForm, setShowRegForm] = useState(false)
     const [showSignIn, setShowSignIn] = useState(false)
-    const handleRegister = () => {
-        setShowRegForm(true)
+
+    const handleRegisterClick = () => {
         setShowSignIn(false)
+        setShowRegForm(true)
+    }
+
+    const handleRegisterSubmit = (e) => {
+        e.preventDefault()
+        console.log('email -', regEmailRef.current.value, 'passwrd - ', regPasswordRef.current.value)
+        setShowRegForm(false)
     }
 
     const handleSignIn = () => {
         setShowSignIn(true)
         setShowRegForm(false)
+    }
+
+    const handleClose = () => {
+        setShowRegForm(false)
+        setShowSignIn(false)
     }
 
     return (
@@ -25,52 +37,56 @@ const HomePage = () => {
                 <h4>Pradėk naują treniruotę, sek savo kūno apimtis arba peržiūrėk pažangą.</h4>
             </div>
             <div className='buttons'>
-                <button onClick={handleRegister}>Registruotis</button>
+                <button onClick={handleRegisterClick}>Registruotis</button>
                 <button onClick={handleSignIn}>Prisijungti</button>
             </div>
             { showRegForm && (
-                <div className='registration'>
-                    <form action="onSubmit">
-                        <div class="mb-3 row">
-                            <label for="staticEmail" class="col-sm-2 col-form-label">Įveskite el. paštą</label>
-                            <div class="col-sm-10">
-                                <input type="text" ref={regEmailRef} readonly class="form-control-plaintext" id="staticEmail" value="email@pavyzdys.com"/>
-                            </div>
-                        </div>
-                        <div class="mb-3 row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Sukurkite slaptažodį: </label>
-                            <div class="col-sm-10">
-                                <input type="password" ref={regPasswordRef} class="form-control" id="inputPassword" aria-describedby="passwordHelpBlock"/>
-                                <div id="passwordHelpBlock" class="form-text">
-                                    Slaptažodį turi sudaryti 8-20 simbolių , gali būti raidės ar skaičiai. Be tarpų, specialiųjų simbolių ar emoji.
+                <div className='overlay' onClick={handleClose}>
+                    <div className='registration' onClick={(e) => e.stopPropagation()}>
+                        <form onSubmit={handleRegisterSubmit}>
+                            <div class="mb-3 row">
+                                <label for="staticEmail" class="col-sm-2 col-form-label">Įveskite el. paštą</label>
+                                <div class="col-sm-10">
+                                    <input type="text" ref={regEmailRef} readonly class="form-control-plaintext" id="staticEmail" value="email@pavyzdys.com"/>
                                 </div>
                             </div>
-                        </div>
-                        <div class="mb-3 row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Pakartokite slaptažodį: </label>
-                            <div class="col-sm-10">
-                                <input type="password" ref={regPasswordRef} class="form-control" id="inputPassword" aria-describedby="passwordHelpBlock"/>
+                            <div class="mb-3 row">
+                                <label for="inputPassword" class="col-sm-2 col-form-label">Sukurkite slaptažodį: </label>
+                                <div class="col-sm-10">
+                                    <input type="password" ref={regPasswordRef} class="form-control" id="inputPassword" aria-describedby="passwordHelpBlock"/>
+                                    <div id="passwordHelpBlock" class="form-text">
+                                        Slaptažodį turi sudaryti 8-20 simbolių , gali būti raidės ar skaičiai. Be tarpų, specialiųjų simbolių ar emoji.
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-primary">Registruotis</button>
-                        </div>
-                    </form>
+                            <div class="mb-3 row">
+                                <label for="inputPassword" class="col-sm-2 col-form-label">Pakartokite slaptažodį: </label>
+                                <div class="col-sm-10">
+                                    <input type="password" ref={regPasswordRef} class="form-control" id="inputPassword" aria-describedby="passwordHelpBlock"/>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary">Registruotis</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             )}
 
             {showSignIn && (
-                <div className='sign-in-field'>
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-                        <label for="floatingInput">El. paštas</label>
-                    </div>
-                    <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-                        <label for="floatingPassword">Slaptažodis</label>
-                    </div>
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-primary">Įeiti</button>
+                <div className='overlay' onClick={handleClose}>
+                    <div className='sign-in-field' onClick={(e) => e.stopPropagation()}>
+                        <div class="form-floating mb-3">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+                            <label htmlfor="floatingInput">El. paštas</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
+                            <label htmlfor="floatingPassword">Slaptažodis</label>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary">Įeiti</button>
+                        </div>
                     </div>
                 </div>
             )}
