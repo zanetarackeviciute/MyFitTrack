792fa52ef148bf78912f590fdaa45701fb4063fe
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-07-17 11:02:36 +0300

    Add Name, change class to className and for to htmlFor

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,9 @@ import './HomePage.scss'
 // import logo from '../assets/MyFitTrackLogo.png'
 const HomePage = () => {
     const regPasswordRef = useRef()
+    const regPasswordRepeatRef = useRef()
     const regEmailRef = useRef()
+    const regNameRef = useRef()
 
     const [showRegForm, setShowRegForm] = useState(false)
     const [showSignIn, setShowSignIn] = useState(false)
@@ -44,29 +46,38 @@ const HomePage = () => {
                 <div className='overlay' onClick={handleClose}>
                     <div className='registration' onClick={(e) => e.stopPropagation()}>
                         <form onSubmit={handleRegisterSubmit}>
-                            <div class="mb-3 row">
-                                <label for="staticEmail" class="col-sm-2 col-form-label">Įveskite el. paštą</label>
-                                <div class="col-sm-10">
-                                    <input type="text" ref={regEmailRef} readonly class="form-control-plaintext" id="staticEmail" value="email@pavyzdys.com"/>
+                            <div className="mb-3 row">
+                                <h3>Registracija</h3>
+                            </div>
+                            <div className="mb-3 row">
+                                <label htmlFor="inputName" className="col-sm-2 col-form-label">Įveskite vardą</label>
+                                <div className="col-sm-10">
+                                    <input type="text" ref={regNameRef} id="inputName"/>
                                 </div>
                             </div>
-                            <div class="mb-3 row">
-                                <label for="inputPassword" class="col-sm-2 col-form-label">Sukurkite slaptažodį: </label>
-                                <div class="col-sm-10">
-                                    <input type="password" ref={regPasswordRef} class="form-control" id="inputPassword" aria-describedby="passwordHelpBlock"/>
-                                    <div id="passwordHelpBlock" class="form-text">
+                            <div className="mb-3 row">
+                                <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Įveskite el. paštą</label>
+                                <div className="col-sm-10">
+                                    <input type="text" ref={regEmailRef} readOnly className="form-control-plaintext" id="staticEmail" value="email@pavyzdys.com"/>
+                                </div>
+                            </div>
+                            <div className="mb-3 row">
+                                <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Sukurkite slaptažodį: </label>
+                                <div className="col-sm-10">
+                                    <input type="password" ref={regPasswordRef} className="form-control" id="inputPassword" aria-describedby="passwordHelpBlock"/>
+                                    <div id="passwordHelpBlock" className="form-text">
                                         Slaptažodį turi sudaryti 8-20 simbolių , gali būti raidės ar skaičiai. Be tarpų, specialiųjų simbolių ar emoji.
                                     </div>
                                 </div>
                             </div>
-                            <div class="mb-3 row">
-                                <label for="inputPassword" class="col-sm-2 col-form-label">Pakartokite slaptažodį: </label>
-                                <div class="col-sm-10">
-                                    <input type="password" ref={regPasswordRef} class="form-control" id="inputPassword" aria-describedby="passwordHelpBlock"/>
+                            <div className="mb-3 row">
+                                <label htmlFor="inputPasswordRepeat" className="col-sm-2 col-form-label">Pakartokite slaptažodį: </label>
+                                <div className="col-sm-10">
+                                    <input type="password" ref={regPasswordRepeatRef} className="form-control" id="inputPasswordRepeat" aria-describedby="passwordHelpBlock"/>
                                 </div>
                             </div>
-                            <div class="col-12">
-                                <button type="submit" class="btn btn-primary">Registruotis</button>
+                            <div className="col-12">
+                                <button type="submit" className="btn btn-primary">Registruotis</button>
                             </div>
                         </form>
                     </div>
@@ -76,16 +87,16 @@ const HomePage = () => {
             {showSignIn && (
                 <div className='overlay' onClick={handleClose}>
                     <div className='sign-in-field' onClick={(e) => e.stopPropagation()}>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-                            <label htmlfor="floatingInput">El. paštas</label>
+                        <div className="form-floating mb-3">
+                            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+                            <label htmlFor="floatingInput">El. paštas</label>
                         </div>
-                        <div class="form-floating">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-                            <label htmlfor="floatingPassword">Slaptažodis</label>
+                        <div className="form-floating">
+                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+                            <label htmlFor="floatingPassword">Slaptažodis</label>
                         </div>
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-primary">Įeiti</button>
+                        <div className="col-12">
+                            <button type="submit" className="btn btn-primary">Įeiti</button>
                         </div>
                     </div>
                 </div>
